9a524e210c4b1fea54dd22986c6cec3f044be472
Dusan <dstojnic@zipari.com>
2020-07-09 13:56:47 -0600

    push the thing

--- celery_exporter/metrics.py
+++ celery_exporter/metrics.py
@@ -16,3 +16,39 @@ LATENCY = prometheus_client.Histogram(
 WORKERS = prometheus_client.Gauge(
     "celery_workers", "Number of alive workers", ["namespace"]
 )
+
+STRATEGY_METRIC_CHECKED_IN = Counter(
+    "strategy_checked_in",
+    "Process Strategy Checked In",
+    ['strategy']
+)
+
+STRATEGY_METRIC_INITIALIZED = Counter(
+    "strategy_initialized",
+    "Process Strategy Initialized",
+    ['strategy']
+)
+
+STRATEGY_METRIC_COMPLETED = Counter(
+    "strategy_completed",
+    "Process Strategy Completed",
+    ['strategy']
+)
+
+STRATEGY_METRIC_INVALID = Counter(
+    "strategy_invalid",
+    "Process Strategy Invalid",
+    ['strategy']
+)
+
+STRATEGY_METRIC_DO_NOTHING = Counter(
+    "strategy_do_nothing",
+    "Process Strategy Do Nothing",
+    ['strategy']
+)
+
+STRATEGY_METRIC_ERROR = Counter(
+    "strategy_error",
+    "Process Strategy Error",
+    ['strategy']
+)
