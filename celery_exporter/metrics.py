21ff4791eb0cad3f3b3be554a43dbf80e36a84b8
Dusan <dstojnic@zipari.com>
2020-07-09 14:32:55 -0600

    fix import

--- celery_exporter/metrics.py
+++ celery_exporter/metrics.py
@@ -17,37 +17,37 @@ WORKERS = prometheus_client.Gauge(
     "celery_workers", "Number of alive workers", ["namespace"]
 )
 
-STRATEGY_METRIC_CHECKED_IN = Counter(
+STRATEGY_METRIC_CHECKED_IN = prometheus_client.Counter(
     "strategy_checked_in",
     "Process Strategy Checked In",
     ['strategy']
 )
 
-STRATEGY_METRIC_INITIALIZED = Counter(
+STRATEGY_METRIC_INITIALIZED = prometheus_client.Counter(
     "strategy_initialized",
     "Process Strategy Initialized",
     ['strategy']
 )
 
-STRATEGY_METRIC_COMPLETED = Counter(
+STRATEGY_METRIC_COMPLETED = prometheus_client.Counter(
     "strategy_completed",
     "Process Strategy Completed",
     ['strategy']
 )
 
-STRATEGY_METRIC_INVALID = Counter(
+STRATEGY_METRIC_INVALID = prometheus_client.Counter(
     "strategy_invalid",
     "Process Strategy Invalid",
     ['strategy']
 )
 
-STRATEGY_METRIC_DO_NOTHING = Counter(
+STRATEGY_METRIC_DO_NOTHING = prometheus_client.Counter(
     "strategy_do_nothing",
     "Process Strategy Do Nothing",
     ['strategy']
 )
 
-STRATEGY_METRIC_ERROR = Counter(
+STRATEGY_METRIC_ERROR = prometheus_client.Counter(
     "strategy_error",
     "Process Strategy Error",
     ['strategy']
